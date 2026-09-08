f1b1d665f4c52cc599f5bc5589feb6ba1643720a
NhanDTCE190741 <nhandt.ce190741@gmail.com>
2026-09-08 08:13:20 +0700

    feat: enhance configuration for GitHub Pages deployment and add workflow

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,19 @@
 import type { NextConfig } from "next";
-const config: NextConfig = { devIndicators: false };
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const isGitHubPages =
+  process.env.GITHUB_ACTIONS === "true" && Boolean(repoName);
+
+const config: NextConfig = {
+  devIndicators: false,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  ...(isGitHubPages
+    ? {
+        basePath: `/${repoName}`,
+        assetPrefix: `/${repoName}/`,
+      }
+    : {}),
+};
 export default config;
